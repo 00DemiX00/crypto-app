94aec74323cc2fc691fe0aa334349cecc1dbe9c5
00DemiX00 <alescha.yarkin@yandex.ru>
2025-05-26 21:50:47 +0300

    refactor: декомпозиция, создано 3 новых компонента

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import './App.css'
+import React from 'react';
+import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
 
 
 export default function App () {
-  return
+  return (
+  <Layout>
+    <AppHeader>Header</AppHeader>
+    <Layout>
+      <AppSider>Sider</AppSider>
+      <AppContent>Content</AppContent>
+    </Layout>
+  </Layout>
+      )
 }
